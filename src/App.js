f877752a2fc8e875b72cea3a51baa2cabcb4a485
Aryan Shinde <shindearyan179@gmail.com>
2023-01-23 15:20:41 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import { Route, Routes } from 'react-router';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" exact element="<Home />" />
+        <Route path="/starred" exact element="<Starred />" />
+        <Route path="*" exact element="<NotFound />" />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
