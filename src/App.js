9b95d984cf987e76a4c0c1461e579aed0dbe5114
Aryan Shinde <shindearyan179@gmail.com>
2023-01-23 15:29:30 +0530

    Added Navbar and Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Starred from './pages/Starred';
 
 function App() {
   return (
     <div>
+      <Navs />
       <Routes>
-        <Route path="/" exact element="<Home />" />
-        <Route path="/starred" exact element="<Starred />" />
-        <Route path="*" exact element="<NotFound />" />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/starred" exact element={<Starred />} />
+        <Route path="*" exact element={<NotFound />} />
       </Routes>
     </div>
   );
