ecd0e0b9f8276b72211598bf3cb676be5b9ac275
Aryan Shinde <shindearyan179@gmail.com>
2023-01-23 23:38:10 +0530

    Styled Entire App

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/starred" exact element={<Starred />} />
-      <Route path="/show/:id" exact element={<Show />} />
-      <Route path="*" exact element={<NotFound />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/starred" exact element={<Starred />} />
+        <Route path="/show/:id" exact element={<Show />} />
+        <Route path="*" exact element={<NotFound />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
